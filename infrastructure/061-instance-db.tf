89a698b1765c08b4e29f62cb045940c696327ed9
Gleb Kikot <glebkikot200@gmail.com>
2022-12-26 18:56:43 +0200

    change

--- infrastructure/061-instance-db.tf
+++ infrastructure/061-instance-db.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "db" {
   for_each      = var.db_instance_names
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  key_name      = "gl-Frankfurt"
+  key_name      = aws_key_pair.user_key.key_name
   vpc_security_group_ids = [
     aws_security_group.administration.id,
     aws_security_group.db.id,
