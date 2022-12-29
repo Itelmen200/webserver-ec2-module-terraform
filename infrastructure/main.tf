1fbcc39845a8fec5ac2a97f82cde8da5fa8d5f02
Itelmen200 <72791095+Itelmen200@users.noreply.github.com>
2022-12-29 12:37:57 +0200

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "eu-central-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "project-terraform-tfstate"
+    key    = "prod/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
