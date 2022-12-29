ed97407580b76a9071ee98282ec0868e2458c5f0
Gleb Kikot <glebkikot200@gmail.com>
2022-12-29 13:29:05 +0200

    change

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,5 +7,7 @@ terraform {
     bucket = "project-terraform-tfstate-4531"
     key    = "prod/terraform.tfstate"
     region = "eu-central-1"
+    access_key = "AKIAZQKIXXY7VO5F3B75"
+    secret_key = "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"
   }
 }
